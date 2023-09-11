69c9b7093ab12969aca4b3dbf0f9ae489e6c7ed0
Lucio_Bianchi <luciobianchipradas@gmail.com>
2023-09-11 18:08:00 -0300

    Agregado manejador de 404 y template

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -8,4 +8,8 @@ def create_app(env="development", static_folder="../../static"):
 	def home():
 		return render_template("home.html")
 
+	@app.errorhandler(404)
+	def page_not_found(e):
+		return render_template("404.html"), 404
+
 	return app
