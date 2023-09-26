2cb213be839d8f79a048f4541d3339f43b63050d
Hugo Tonelli <kmikch@yahoo.com.ar>
2023-09-26 20:11:10 -0300

    Agrega librería dev Black

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -3,12 +3,12 @@ from flask import render_template
 from src.web import error
 
 def create_app(env="development", static_folder="../../static"):
-	app = Flask(__name__, static_folder=static_folder)
+    app = Flask(__name__, static_folder=static_folder)
 
-	@app.get("/")
-	def home():
-		return render_template("home.html")
+    @app.get("/")
+    def home():
+        return render_template("home.html")
 
-	app.register_error_handler(404, error.not_found_error)
+    app.register_error_handler(404, error.not_found_error)
 
-	return app
+    return app
