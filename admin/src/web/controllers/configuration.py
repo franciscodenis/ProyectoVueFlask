086ad5bb2e17ece040b2ad3d59d6577f8cb913cc
Lucio Bianchi <luciobianchipradas@gmail.com>
2023-10-16 16:33:05 -0300

    Controlador y bluepritnt de config

---
+++ admin/src/web/controllers/configuration.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from src.core import configuration
+from src.web.forms import ConfigForm
+
+configuration_bp = Blueprint('config', __name__, url_prefix="/config")
+
+@configuration_bp.route("/", methods=['GET', 'POST'])
+def config_update():
+    # Obtén la configuración actual desde la base de datos o donde la tengas almacenada
+    current_config = configuration.get_or_create_config()  # Implementa esta función
+
+    form = ConfigForm(obj=current_config)
+
+    if form.validate_on_submit():
+        # Procesa los datos del formulario y actualiza la configuración
+        new_config = {
+            'items_per_page': form.items_per_page.data,
+            'contact_info': form.contact_info.data,
+            'maintenance_mode': form.maintenance_mode.data,
+            'maintenance_message': form.maintenance_message.data
+        }
+
+        # Llama a una función para actualizar la configuración, implementa esta función
+        result = configuration.update_config(new_config)  
+
+        if result:
+            flash('La configuración se ha actualizado correctamente.', 'flash-message-success')
+            return render_template('home.html')
+        else:
+            flash('Hubo un error al actualizar la configuración.', 'flash-message-error')
+
+    return render_template('config.html', form=form)
