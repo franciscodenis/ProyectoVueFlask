373b5972bcd01d36f8b3e654b8e56a485b350101
Hugo Tonelli <kmikch@yahoo.com.ar>
2023-10-18 01:59:19 -0300

    Agrega registro de usuario y activacion por mail

--- admin/src/web/controllers/auth.py
+++ admin/src/web/controllers/auth.py
@@ -37,3 +37,36 @@ def logout():
         flash("No hay sesión iniciada", "info")
 
     return redirect(url_for("auth.login"))
+
+@auth_bp.get("/validate")
+def validate():
+
+    email = request.args.get("email", None)
+    key = request.args.get("key", None)
+
+    if email and key:
+        if auth.validate_email(email, key):
+            flash("Email validado correctamente, ya puedes iniciar sesión", "success")
+            return redirect(url_for("auth.login"))
+
+    flash("Hubo un error al validar el usuario", "danger")
+    return redirect(url_for("auth.login"))
+
+@auth_bp.get("/register")
+def register():
+    return render_template("auth/register.html")
+
+@auth_bp.post("/register")
+def create():
+    params = request.form
+
+    print(params)
+
+    user = auth.create_user_stub(params["email"], params["first_name"], params["last_name"])
+
+    if not user:
+        flash("No se pudo crear el usuario, verifique los datos", "danger")
+        return redirect(url_for("auth.register"))
+
+    flash("Usuario creado correctamente, se le ha enviado un email para su activación.", "info")
+    return redirect(url_for("auth.login"))
