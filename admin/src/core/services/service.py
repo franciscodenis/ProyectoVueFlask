fc43fac76e606cc6d87e4d91ba8e2f18378654a0
Lucio Bianchi <luciobianchipradas@gmail.com>
2023-10-20 13:03:50 -0300

    Agrego relaciones con service_request

--- admin/src/core/services/service.py
+++ admin/src/core/services/service.py
@@ -11,12 +11,13 @@ class Service(db.Model):
     __tablename__ = 'services'
     id = db.Column(db.Integer, primary_key=True, unique=True)
     institution_id = db.Column(db.Integer, db.ForeignKey('institutions.id'), nullable=False)
-    institution = db.relationship('Institution', back_populates='services')
+    institution = db.relationship('Institution', back_populates='services', lazy=True)
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     keywords = db.Column(db.String(255), nullable=False)
     service_type = db.Column(db.Enum(ServiceType), nullable=False)
     enabled = db.Column(db.Boolean, default=True)
+    service_requests = db.relationship('ServiceRequest', back_populates='service', lazy=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
